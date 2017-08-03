06d0c708d29839e0841182a37477d2583bfd4e57
Manokaran <mohan.manokaran@capgemini.com>
2017-08-03 19:08:11 +0530

    start-build-01

--- server.py
+++ server.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 def root():
     return '''
     <html>
-        <center><h1>This is sample app from a container </h1> </center>
+        <center><h1>This is sample app from a container - 01 </h1> </center>
     </html>
 
     '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090)
-    
